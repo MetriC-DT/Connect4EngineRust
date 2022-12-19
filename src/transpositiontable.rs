b0119d43d1236ab6c51507d97ad952297c389f39
Derick Tseng <dericktseng@tutanota.com>
2022-12-18 20:42:40 -0800

    update transposition table to only contain one 64 bit number

--- src/transpositiontable.rs
+++ src/transpositiontable.rs
@@ -3,23 +3,43 @@ use crate::board::Board;
 /// Number of elements in the table. Best to choose a prime.
 const MAX_TABLE_SIZE: usize = (1 << 19) - 1;
 
+/// bits in playable region (refer to board.rs)
+const PLAYABLE_BITS: i64 = 56;
+
+/// mask for the playable region
+const KEY_BIT_MASK: i64 = (1 << PLAYABLE_BITS) - 1;
+
+/// whatever isn't the key mask will be the evaluation.
+const EVAL_BIT_MASK: i64 = !KEY_BIT_MASK;
+
 /// represents an entry of the transposition table.
 #[derive(Debug, Clone)]
 struct Entry {
-    board_key: i64,
-    evaluation: i8
+    storage: i64
 }
 
 impl Entry {
     pub fn new(board_key: i64, evaluation: i8) -> Self {
-        Self { board_key, evaluation }
+        // we don't need to mask anything since board is guaranteed to not
+        // have anything in the bits above the 53.
+        let mut storage = board_key;
+        storage |= (evaluation as i64) << PLAYABLE_BITS;
+        Self { storage }
+    }
+
+    pub fn get_key(&self) -> i64 {
+        self.storage & KEY_BIT_MASK
+    }
+
+    pub fn get_eval(&self) -> i8 {
+        let eval = (self.storage & EVAL_BIT_MASK) >> PLAYABLE_BITS;
+        eval.try_into().unwrap()
     }
 }
 
 impl Default for Entry {
     fn default() -> Self {
-        // chose to use -1 (0b11111...) because no key will have that value.
-        Entry::new(-1, 0)
+        Entry::new(0, 0)
     }
 }
 
@@ -54,8 +74,8 @@ impl TranspositionTable {
         let loc = TranspositionTable::location(key);
         let selected_entry = &self.table[loc];
 
-        if selected_entry.board_key == key {
-            Some(selected_entry.evaluation)
+        if selected_entry.get_key() == key {
+            Some(selected_entry.get_eval())
         }
         else {
             None
