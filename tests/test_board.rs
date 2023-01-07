c410af2683b33d71b31e79258cf8ead07009ab8d
Derick Tseng <dericktseng@tutanota.com>
2023-01-06 21:38:38 -0800

    update licence years

--- tests/test_board.rs
+++ tests/test_board.rs
@@ -1,4 +1,21 @@
-use connect4engine::{board::{HEIGHT, Board, WIDTH, BOTTOM_ROW_MASK, COLUMN_MASK, COUNTS_PER_COL}, moves::{DEFAULT_ORDER, Moves}};
+// Connect4EngineRust, a strong solver for the connect-4 board game.
+// Copyright (C) 2023 Derick Tseng
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use connect4engine::moves::{DEFAULT_ORDER, Moves};
+use connect4engine::board::{HEIGHT, Board, WIDTH, BOTTOM_ROW_MASK, COLUMN_MASK, COUNTS_PER_COL};
 
 fn test_winning_line(line: &str) {
     let mut b = Board::new();
--- tests/test_transpositiontable.rs
+++ tests/test_transpositiontable.rs
@@ -1,3 +1,19 @@
+// Connect4EngineRust, a strong solver for the connect-4 board game.
+// Copyright (C) 2023 Derick Tseng
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use connect4engine::board::Board;
 use connect4engine::transpositiontable::MAX_TABLE_SIZE;
 use connect4engine::transpositiontable::TranspositionTable;
